9a8ce35020c912c536eaf41706b825d5064c6b19
Hung Tran <hung.tran@swissitbridge.com>
2017-12-07 16:14:32 +0700

    able to slide in contents when scrolling down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Renderer } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
+    title = 'app';
+    numbers = Array(50).map((x, i) => i);
+
+    constructor(private elementRef: ElementRef, private renderer: Renderer) {
+    }
+
+    ngOnInit() {
+
+    }
+
+    ngAfterViewInit() {
+        let allMods = this.elementRef.nativeElement.querySelector('section').children;
+        for (let module of allMods) {
+            if (this.isVisible(module, true)) {
+                this.renderer.setElementClass(module, 'already-visible', true);
+            }
+        }
+    }
+
+    isVisible(el:any, partial: boolean): boolean {
+        let viewTop = document.documentElement.scrollTop;
+        let viewBottom = viewTop + document.documentElement.clientHeight;
+        let top = el.offsetTop;
+        let bottom = top + el.offsetHeight;
+        let compareTop = partial === true ? bottom : top;
+        let compareBottom = partial === true ? top : bottom;
+
+        console.log('view top', viewTop, 'view bottom', viewBottom, 'top', top, 'bottom', bottom);
+
+        return ((compareBottom <= viewBottom) && (compareTop >= viewTop));
+    }
+
+    @HostListener('window:scroll', ['$event']) onScrollEvent($event) {
+        // console.log($event);
+        let allMods = this.elementRef.nativeElement.querySelector('section').children;
+        for (let module of allMods) {
+            if (this.isVisible(module, true)) {
+                this.renderer.setElementClass(module, 'come-in', true);
+            }
+        }
+    }
 }
